e99f1d144f55ba96f9321c2c4e6aabc437e93744
sridewiangraeni <114473779+sridewiangraeni@users.noreply.github.com>
2023-03-09 21:17:13 +0700

    Add files via upload

---
+++ pertemuan 2/praktik 2/hasil_daftar.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Data Pendaftar</title>
+	<!-- Load Bootstrap CSS -->
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+<body>
+	<div class="container">
+		<h2>Data Pendaftar</h2>
+		<table class="table">
+			<thead>
+				<tr>
+					<th>No</th>
+					<th>Nama Lengkap</th>
+					<th>Email</th>
+					<th>Alamat</th>
+					<th>Telepon</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<td>Sri Dewi Angraeni</td>
+					<td>sridewia711@gmail,com</td>
+					<td>Bogor</td>
+					<td>+62895393387092</td>
+				</tr>
+				<tr>
+					<td>dew</td>
+					<td>dew@gmail.com</td>
+					<td>Bogor</td>
+					<td>+62895393387092</td>
+				</tr>
+
+				<?php
+				  /* Task 2
+				    Tangkap dan tampilkan data dari form_daftar.
+						Berikan error handling
+				  */
+				?>
+
+			</tbody>
+		</table>
+	</div>
+</body>
+</html>
